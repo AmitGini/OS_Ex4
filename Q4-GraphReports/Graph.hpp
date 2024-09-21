50567e9153710d5bdcecad96c02c2c0313c406bc
Amit Gini <amit.gini@msmail.ariel.ac.il>
2024-09-21 12:41:25 +0300

    Updates

---
+++ Q4-GraphReports/Graph.hpp
@@ -0,0 +1,21 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <vector>
+
+class Graph
+{
+private:
+    int vertices;
+    std::vector<std::vector<int>> adjMatrix;
+    bool isEulerian();
+    void DFS(int v, std::vector<bool> &visited);
+
+public:
+    Graph(int vertices);
+    ~Graph();
+    void addEdge(int u, int v);
+    void eulerianCycle();
+};
+
+#endif
